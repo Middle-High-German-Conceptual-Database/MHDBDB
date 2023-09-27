8bbd1f2c5dd81b7c42c599565ee8f8710bf6468b
Daniel Schlager <email@danielschlager.at>
2023-09-27 21:45:22 +0200

    Added encodingDesc to TEI Service

--- backend/src/main/java/at/ac/plus/mhdbdb/backend/TeiViewController.java
+++ backend/src/main/java/at/ac/plus/mhdbdb/backend/TeiViewController.java
@@ -9,8 +9,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.StringWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
 @RestController
 @CrossOrigin(origins = { "${app.dev.frontend.local}", "${app.dev.frontend.remote}", "${app.dev.frontend.remote2}" })
@@ -21,14 +34,35 @@ public class TeiViewController {
     @Value("${app.dev.frontend.teiFolder}")
     private String teiFolder;
 
+    // XSLT Helper File
+    @Value("${app.dev.frontend.additionalXmlPath}")
+    private String additionalXmlPath;
+
     @GetMapping(value = "/showTei", produces = MediaType.APPLICATION_XML_VALUE)
     public String showTei(@RequestParam String id) {
         try {
+
             // Read the XML file from the server
             String xmlFilePath = teiFolder + "/" + id + ".tei.xml"; // Update with the actual file path
-            String xmlContent = new String(Files.readAllBytes(Paths.get(xmlFilePath)));
-
             logger.info("Open " + xmlFilePath);
+
+            Document mainDoc = loadXmlDocument(xmlFilePath);
+
+            // Read and parse the additional XML file
+            Document additionalDoc = loadXmlDocument(additionalXmlPath);
+
+            // Append the content of the additional XML to the teiHeader of the main XML
+            Node teiHeader = mainDoc.getElementsByTagName("tei:teiHeader").item(0);
+            Node importedNode = mainDoc.importNode(additionalDoc.getDocumentElement(), true);
+            teiHeader.appendChild(importedNode);
+
+            // Serialize the modified XML back to string
+            TransformerFactory tf = TransformerFactory.newInstance();
+            Transformer transformer = tf.newTransformer();
+            StringWriter writer = new StringWriter();
+            transformer.transform(new DOMSource(mainDoc), new StreamResult(writer));
+
+            String xmlContent = writer.getBuffer().toString();
 
             // Split the XML content by new lines
             String[] lines = xmlContent.split("\n", 2);
@@ -52,6 +86,12 @@ public class TeiViewController {
             // Handle exceptions accordingly
             return null; // Replace with appropriate error handling
         }
+    }
+
+     private Document loadXmlDocument(String filePath) throws Exception {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        return builder.parse(new File(filePath));
     }
 
 }
