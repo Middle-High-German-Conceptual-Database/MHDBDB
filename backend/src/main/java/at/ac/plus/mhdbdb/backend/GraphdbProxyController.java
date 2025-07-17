87a77c2f66a79938c2eeebc89ed8cff3386c814e
Clemens Hafner <clemens.hafner@complement.at>
2025-07-18 00:15:26 +0200

    start refactoring the proxy

--- backend/src/main/java/at/ac/plus/mhdbdb/backend/GraphdbProxyController.java
+++ backend/src/main/java/at/ac/plus/mhdbdb/backend/GraphdbProxyController.java
@@ -17,10 +17,20 @@ import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+
+import com.ontotext.graphdb.repository.http.GraphDBHTTPRepository;
+import com.ontotext.graphdb.repository.http.GraphDBHTTPRepositoryBuilder;
+import org.eclipse.rdf4j.query.*;
+// use this fully qualified, as Value collides with org.springframework.beans.factory.annotation.Value;
+//import org.eclipse.rdf4j.model.Value;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
 
 @RestController
 @CrossOrigin(origins = { "${app.dev.frontend.local}", "${app.dev.frontend.remote}", "${app.dev.frontend.remote2}", "${app.dev.frontend.remote3}", "${app.dev.frontend.remote4}", "${app.dev.frontend.remote5}", "${app.dev.frontend.remote6}" })
@@ -34,6 +44,9 @@ public class GraphdbProxyController {
     @Value("${target.host}")
     private String targetHost;
 
+    @Value("${target.repository}")
+    private String targetRepository;
+
     private Map<String, String> getHeaders(HttpServletRequest request) {
         Map<String, String> headers = new HashMap<>();
         Enumeration<String> headerNames = request.getHeaderNames();
@@ -45,6 +58,52 @@ public class GraphdbProxyController {
         return headers;
     }
 
+    @RequestMapping(value = "/repositories/dhPLUS/**", method = { RequestMethod.GET, RequestMethod.POST })
+    public @ResponseBody String proxy(HttpServletRequest request, @RequestBody(required = false) String body) {
+        String requestUrl = targetHost + request.getRequestURI();
+
+        // Log the request URI and method
+        logger.info("Request URI: " + request.getRequestURI());
+        logger.info("Request Method: " + request.getMethod());
+        // taken from https://graphdb.ontotext.com/documentation/11.0/helloworld.html
+
+        GraphDBHTTPRepository repository = new GraphDBHTTPRepositoryBuilder()
+            .withServerUrl(targetHost)
+            .withRepositoryId(targetRepository)
+            .build();
+        RepositoryConnection connection = repository.getConnection();
+
+        TupleQuery tupleQuery = connection.prepareTupleQuery(QueryLanguage.SPARQL, body);
+        List<BindingSet> bindings = new ArrayList<BindingSet>();
+        TupleQueryResult tupleQueryResult = tupleQuery.evaluate();
+            while (tupleQueryResult.hasNext()) {
+                // Each result is represented by a BindingSet, which corresponds to a result row
+                BindingSet bindingSet = tupleQueryResult.next();
+                bindings.add(bindingSet);
+            }
+
+            // Once we are done with a particular result we need to close it
+            tupleQueryResult.close();        
+        // Convert the response body to a JSON object
+        Object responseBody;
+        try {
+            //responseBody = new ObjectMapper().readValue(response.getBody(), Object.class);
+            responseBody = new ObjectMapper().writeValueAsString(bindings);
+            /* from reo
+            ObjectMapper om = new ObjectMapper();
+            JSONObject model = new JSONObject();
+            model.put("attachments", new JSONArray(om.writeValueAsString(attachments)));
+            */
+        } catch (JsonProcessingException e) {
+            // logger.error("Error parsing response body to JSON", e);
+            responseBody = Collections.singletonMap("error", "Failed to parse response body");
+        }
+
+        // Return the response from the target host
+        //return ResponseEntity.status(response.getStatusCode()).headers(response.getHeaders()).body(responseBody);
+        return responseBody.toString();
+    }
+    /*
     @RequestMapping(value = "/repositories/dhPLUS/**", method = { RequestMethod.GET, RequestMethod.POST })
     public ResponseEntity<Object> proxy(HttpServletRequest request, @RequestBody(required = false) String body) {
         String requestUrl = targetHost + request.getRequestURI();
@@ -97,5 +156,5 @@ public class GraphdbProxyController {
         // Return the response from the target host
         return ResponseEntity.status(response.getStatusCode()).headers(response.getHeaders()).body(responseBody);
     }
-
+    */
 }
