60db167248e9435b1d94c3028e76c9755abe54f9
Clemens Hafner <clemens.hafner@complement.at>
2025-07-23 12:35:45 +0200

    fix controller

--- backend/src/main/java/at/ac/plus/mhdbdb/backend/ControllerBase.java
+++ backend/src/main/java/at/ac/plus/mhdbdb/backend/ControllerBase.java
@@ -3,9 +3,6 @@ package at.ac.plus.mhdbdb.backend;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@CrossOrigin(origins = { "${app.dev.frontend.local}", "${app.dev.frontend.remote}", "${app.dev.frontend.remote2}", "${app.dev.frontend.remote3}", "${app.dev.frontend.remote4}", "${app.dev.frontend.remote5}", "${app.dev.frontend.remote6}" })
-@RequestMapping("/api")
 public class ControllerBase {
 
     @Value("${target.host}")
--- backend/src/main/java/at/ac/plus/mhdbdb/backend/WorkController.java
+++ backend/src/main/java/at/ac/plus/mhdbdb/backend/WorkController.java
@@ -20,7 +20,9 @@ import org.eclipse.rdf4j.query.resultio.sparqljson.SPARQLResultsJSONWriter;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.json.JSONException;
 
-@RequestMapping("/works")
+@RestController
+@CrossOrigin(origins = { "${app.dev.frontend.local}", "${app.dev.frontend.remote}", "${app.dev.frontend.remote2}", "${app.dev.frontend.remote3}", "${app.dev.frontend.remote4}", "${app.dev.frontend.remote5}", "${app.dev.frontend.remote6}" })
+@RequestMapping("/api/v1/works")
 public class WorkController extends ControllerBase {
     // TODO: This Controller shall list works ('search') and return details ('detail') for works
 
@@ -29,7 +31,7 @@ public class WorkController extends ControllerBase {
     @RequestMapping(value = "/search", method = {RequestMethod.POST, RequestMethod.GET}, produces = "application/json")
     public @ResponseBody void search(HttpServletResponse response, HttpServletRequest request, @RequestBody(required = false) String body) 
     throws JSONException, IOException {
-        logger.info("WorkController.proxy start");
+        logger.info("WorkController.proxy start!");
 
 
         GraphDBHTTPRepository repository = new GraphDBHTTPRepositoryBuilder()
