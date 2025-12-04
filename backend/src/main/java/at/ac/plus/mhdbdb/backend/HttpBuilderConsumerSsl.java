5a41b15e5785d1ef484d14c610102531208e1543
Clemens Hafner <clemens.hafner@complement.at>
2025-12-04 17:36:00 +0100

    add HTTP Basic Auth to call
    The credentials target.username and target.password are not checked in.
    We'll require some kind of secrets management for that

--- backend/src/main/java/at/ac/plus/mhdbdb/backend/HttpBuilderConsumerSsl.java
+++ backend/src/main/java/at/ac/plus/mhdbdb/backend/HttpBuilderConsumerSsl.java
@@ -6,6 +6,10 @@ import java.util.function.Consumer;
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
 
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.ssl.TrustStrategy;
@@ -28,27 +32,44 @@ public class HttpBuilderConsumerSsl implements Consumer<HttpClientBuilder>
     @Value("${trust.store.password:}")
     protected String trustStorePassword;
 
+    @Value("${target.password:}")
+    protected String targetPassword;
+
+    @Value("${target.username:}")
+    protected String targetUsername;
+
     @Autowired 
     HostnameVerifier hostnameVerifier;
 
     private static final Logger logger = LoggerFactory.getLogger(HttpBuilderConsumerSsl.class);
 
     @Override
-    public void accept(HttpClientBuilder t) {
-        if (trustStore == null || trustStore.equals("")) {
+    public void accept(HttpClientBuilder builder) {
+        if ((trustStore == null || trustStore.equals("")) && (targetPassword == null || targetPassword.equals(""))) {
             return;
         }
-        try {
+        // https://stackoverflow.com/questions/20914311/httpclientbuilder-basic-auth
+        if (targetUsername!= null && !targetUsername.equals("") && targetPassword!= null && !targetPassword.equals("")) {
+            CredentialsProvider provider = new BasicCredentialsProvider();
+            AuthScope scope = new AuthScope(AuthScope.ANY_HOST, AuthScope.ANY_PORT, AuthScope.ANY_REALM);
+            UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(targetUsername, targetPassword);
+            provider.setCredentials(scope, credentials);
+            builder.setDefaultCredentialsProvider(provider);
 
-            TrustStrategy trustStrategy = (X509Certificate[] chain, String authType) -> true;
-            SSLContext sslContext = new SSLContextBuilder()
-                .loadTrustMaterial(trustStore.getURL(), trustStorePassword.toCharArray(), trustStrategy)
-                .build();
-
-            t.setSSLContext(sslContext);
-            t.setSSLHostnameVerifier(hostnameVerifier);
-        } catch(Exception ex) {
-            logger.error("Error adding SSL context to Http Client", ex);
+        }
+        if (trustStore != null && !trustStore.equals("")) {
+            try {
+    
+                TrustStrategy trustStrategy = (X509Certificate[] chain, String authType) -> true;
+                SSLContext sslContext = new SSLContextBuilder()
+                    .loadTrustMaterial(trustStore.getURL(), trustStorePassword.toCharArray(), trustStrategy)
+                    .build();
+    
+                builder.setSSLContext(sslContext);
+                builder.setSSLHostnameVerifier(hostnameVerifier);
+            } catch(Exception ex) {
+                logger.error("Error adding SSL context to Http Client", ex);
+            }
         }
     }
 }
