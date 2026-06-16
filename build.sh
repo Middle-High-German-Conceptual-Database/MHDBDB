d6af565145c2cb39d6ff8206c1a387d1f5ec3292
Clemens Hafner <clemens.hafner@complement.at>
2026-06-16 16:57:17 +0200

    use dhInfra as SPARQL endpoint by default

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ sdk use java 21.0.9-tem
 cd backend
 mvn clean install
 cd ..
+# for original GraphDB on the host:
 docker compose stop && docker compose build && docker compose up -d
 # for connecting to the DHInfra sparql endpoint:
-# docker compose stop && docker compose build && docker compose --env-file docker-compose-dhinfra.env up -d
-
+docker compose stop && docker compose build && docker compose --env-file docker-compose-dhinfra.env up -d
