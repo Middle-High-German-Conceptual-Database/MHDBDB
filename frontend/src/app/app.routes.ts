9c9beb4b68ba4016cbc21ac5df8d91b160922608
Clemens Hafner <clemens.hafner@complement.at>
2026-06-25 20:09:55 +0200

    add optional searchterm to tei route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,6 +8,7 @@ export const routes: Routes = [
   { path: '', component: AppHomeComponent },
   { path: 'home', component: AppHomeComponent },
   { path: 'view/:folder/:filename', component: MarkdownDisplayComponent, data: {}, runGuardsAndResolvers: 'always'  },
+  { path: 'tei/:id', component: TeiWidgetComponent, data: {}, runGuardsAndResolvers: 'always'  },
   { path: 'tei/:id/:searchterm', component: TeiWidgetComponent, data: {}, runGuardsAndResolvers: 'always'  },
   {
     path: 'reference',
